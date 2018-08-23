e5aaf780c3125826f0b1ea47e8a9c35b89c378fb
bmh10 <bmhomer13@gmail.com>
2018-08-23 18:14:50 +0100

    natas upto lvl4

---
+++ natas.sh
@@ -0,0 +1,20 @@
+#0
+# view page source
+#pwd: natas0
+
+#1
+# view page source Ctrl-U
+#pwd: gtVrDuiDfck831PqWsLEZy5gyDz1clto
+
+#2
+# go to /files/users.txt
+#pwd: luruAthQk7Q2MqmDeTiUij2ZvWy2mBi
+
+#3
+# view robots.txt
+# then go to /s3cr3t
+#pwd: sJIJNW6ucpu6HPZ1ZAchaDtwd7oGrD14
+
+#4
+
+#pwd: Z9tkRkWmpt9Qr7XrR5jWRkgOU901swEZ
