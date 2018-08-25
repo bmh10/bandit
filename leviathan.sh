525417ecd720de7ee85541c7901051de15f303bc
bmh10 <bmhomer13@gmail.com>
2018-08-25 13:32:40 +0100

    leviathan1

--- leviathan.sh
+++ leviathan.sh
@@ -3,5 +3,11 @@ less .backup/bookmarks.html
 #pwd: leviathan0
 
 #1
-
+ltrace ./check
+# Use pwd givent to get shell access as leviathan2
+cat /etc/leviathan_pass/leviathan2
 #pwd: rioGegei8m
+
+#2
+
+#pwd: ougahZi8Ta
